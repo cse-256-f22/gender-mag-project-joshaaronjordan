0d5c96474067ba24e61403fd85d6524e52a47e82
attaylor20 <97982206+attaylor20@users.noreply.github.com>
2022-11-09 11:01:29 -0600

    Added side panel

--- view.js
+++ view.js
@@ -72,3 +72,60 @@ $('.permbutton').click( function( e ) {
 
 // ---- Assign unique ids to everything that doesn't have an ID ----
 $('#html-loc').find('*').uniqueId() 
+
+
+
+/*
+Studio code
+*/
+var new_user_permission = define_new_effective_permissions('permissions_panel', true, null);
+$('#sidepanel').append(new_user_permission);
+
+
+//Create a user selection button
+// var selection_panel = define_new_user_select_field('new_user', 'Select User', function(selected_user) { 
+//     $('#permissions_panel').attr('username', selected_user)
+//     $('#permissions_panel').attr('filepath', 'C/presentation_documents/important_file.txt_permicon')
+// });
+
+
+
+var select_panel = define_new_user_select_field('selector_panel', 'Select User', on_user_change = function(selected_user) {
+    $('#permissions_panel').attr('username', selected_user)
+    $('#permissions_panel').attr('filepath', '/C/presentation_documents/important_file.txt')
+ });
+$('#sidepanel').append(select_panel);
+
+
+//Digalog(ue)
+var new_dialog = define_new_dialog('dialog', title='', options = {})
+
+
+$('.perm_info').click(function(){
+    new_dialog.dialog('open');
+    console.log($('#permissions_panel').attr('filepath'), $('#permissions_panel').attr('username'), $(this).attr('permission_name'));
+
+    var file_obj = path_to_file[$('#permissions_panel').attr('filepath')];
+    var user_obj = all_users[$('#permissions_panel').attr('username')];
+    const explanations = allow_user_action(file_obj, user_obj, $(this).attr('permission_name'), explain_why = true);
+    var explanation_text = get_explanation_text(explanations);
+    $('#dialog').text(explanation_text);
+
+})
+
+//Need to fix GetExplanationText object I think is last thing
+
+
+
+
+// //Pull explanation
+// allow_user_action(my_file_obj_var, my_user_obj_var, permission_to_check, explain_why = false);
+
+// var my_file_obj_var = path_to_file['/C/presentation_documents/important_file.txt'];
+
+// var my_user_obj_var = all_users[selected_user];
+
+
+
+// var explanation_text = get_explanation_text(explain_why);
+// $('#dialog').text(explanation_text) //NEED TO USE THIS TO SELECT THE CORRECT ELEMENT IN THE DIALOG
